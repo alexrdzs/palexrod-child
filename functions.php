b352ef5740caea240cde0d182445331e8267a0d8
alex rodríguez santibáñez <alex@alexrds.com>
2018-02-25 22:18:54 +0100

    Quitar las query strings - Functions

--- functions.php
+++ functions.php
@@ -23,3 +23,11 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+//Quitar las query strings from statics resources
+function _remove_script_version( $src ){
+$parts = explode( '?ver', $src );
+return $parts[0];
+}
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
