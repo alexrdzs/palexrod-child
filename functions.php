e80aa9f644d6842ed8472ccb1ca55245dd368549
alex rodríguez santibáñez <alex@alexrds.com>
2018-03-08 14:24:01 -0600

    Generic ChildTheme

--- functions.php
+++ functions.php
@@ -1,33 +1,16 @@
-<?php
-/**
- * Astra - Palexrod Theme functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package Astra - Palexrod
- * @since 1.0.0
- */
+<?php 
+	 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+	 function my_theme_enqueue_styles() { 
+ 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+ 		  } 
 
-/**
- * Define Constants
- */
-define( 'CHILD_THEME_ASTRA_PALEXROD_VERSION', '1.0.0' );
-
-/**
- * Enqueue styles
- */
-function child_enqueue_styles() {
-
-	wp_enqueue_style( 'astra-palexrod-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_PALEXROD_VERSION, 'all' );
-
-}
-
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
 //Quitar las query strings from statics resources
 function _remove_script_version( $src ){
-$parts = explode( '?ver', $src );
+$parts = explode( '?', $src );
 return $parts[0];
 }
 add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
 add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+
+ ?>
